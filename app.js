d1d23923070f7ddf416b6352d5f8a70c2d87f6f5
Jared <jared@sonic.lan>
2023-04-05 00:21:45 -0600

    fixed css

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const campusURL = 'http://10.5.102.146:1234/';
 const globalURL = 'http://147.185.221.212:41897';
 
+function setDarkMode(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+   }
+   
+   // Check the user's system preference for dark mode
+   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+   setDarkMode(prefersDarkScheme.matches);
+   
+   // Listen for changes in the user's system preference
+   prefersDarkScheme.addListener((event) => {
+    setDarkMode(event.matches);
+   });
+
 async function checkURLAvailability(url) {
   try {
     const response = await fetch(url, { mode: 'no-cors' });
