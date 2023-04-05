537ebcf543a9875303b425983a0ee910b35a4a78
Jared <jared@sonic.local>
2023-04-05 02:08:03 -0600

    this probably isn't the solution

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
 const campusURL = 'http://10.5.102.146:1234/';
 const globalURL = 'http://147.185.221.212:41897';
 
-const timeoutDuration = 500; // Adjust this value to change the timeout (in milliseconds)
+const checkImage = 'check.png'; // A small image file you need to add to your Flask app
+const timeoutDuration = 5; // Adjust this value to change the timeout (in milliseconds)
 
-function checkURLAvailability(url, timeout) {
+function loadImage(url, timeout) {
   return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => reject(new Error('Request timed out')), timeout);
+    const timer = setTimeout(() => {
+      img.onerror = img.onload = null;
+      reject(new Error('Request timed out'));
+    }, timeout);
 
-    fetch(url, { mode: 'no-cors' })
-      .then(response => {
-        clearTimeout(timer);
-        resolve(response.ok);
-      })
-      .catch(error => {
-        clearTimeout(timer);
-        reject(error);
-      });
+    const img = new Image();
+    img.onerror = () => {
+      clearTimeout(timer);
+      reject(new Error('Image failed to load'));
+    };
+    img.onload = () => {
+      clearTimeout(timer);
+      resolve();
+    };
+    img.src = url;
   });
 }
 
-async function redirectToApp() {
+(async function redirectToApp() {
   try {
-    await checkURLAvailability(campusURL, timeoutDuration);
+    await loadImage(`${campusURL}/${checkImage}`, timeoutDuration);
     window.location.href = campusURL;
   } catch (error) {
-    // If campusURL fails, try the globalURL
-    try {
-      await checkURLAvailability(globalURL, timeoutDuration);
-      window.location.href = globalURL;
-    } catch (globalError) {
-      // Show an error message if both URLs fail
-      document.querySelector('h1').textContent = 'Unable to access both campus and global instances.';
-    }
+    window.location.href = globalURL;
   }
-}
-
-redirectToApp();
+})();
