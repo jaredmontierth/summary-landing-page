bf309680b0d10708552ba72fd7ea8413ff0320ca
Jared <jared@sonic.local>
2023-04-05 01:16:29 -0600

    trying with check.png

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const campusURL = 'http://10.5.102.146:1234/';
 const globalURL = 'http://147.185.221.212:41897';
+
+const checkImage = 'check.png'; // A small image file you need to add to your Flask app
 const timeoutDuration = 5; // Adjust this value to change the timeout (in milliseconds)
 
-function checkURLAvailability(url, timeout) {
+function loadImage(url, timeout) {
   return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => reject(new Error('Request timed out')), timeout);
+    const timer = setTimeout(() => {
+      img.onerror = img.onload = null;
+      reject(new Error('Request timed out'));
+    }, timeout);
 
-    fetch(url, { mode: 'no-cors' })
-      .then(response => {
-        clearTimeout(timer);
-        resolve(response.ok);
-      })
-      .catch(error => {
-        clearTimeout(timer);
-        reject(error);
-      });
+    const img = new Image();
+    img.onerror = () => {
+      clearTimeout(timer);
+      reject(new Error('Image failed to load'));
+    };
+    img.onload = () => {
+      clearTimeout(timer);
+      resolve();
+    };
+    img.src = url;
   });
 }
 
 (async function redirectToApp() {
   try {
-    await checkURLAvailability(campusURL, timeoutDuration);
+    await loadImage(`${campusURL}/${checkImage}`, timeoutDuration);
     window.location.href = campusURL;
   } catch (error) {
     window.location.href = globalURL;
