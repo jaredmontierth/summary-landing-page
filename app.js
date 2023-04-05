08846c32b5408d7ed83e6c2576e2871a02c6b1f2
Jared <jared@sonic.lan>
2023-04-05 00:42:06 -0600

    making landing page quicker

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const campusURL = 'http://10.5.102.146:1234/';
 const globalURL = 'http://147.185.221.212:41897';
+const timeoutDuration = 50; // Adjust this value to change the timeout (in milliseconds)
 
-async function checkURLAvailability(url) {
-  try {
-    const response = await fetch(url, { mode: 'no-cors' });
-    return response.ok;
-  } catch (error) {
-    return false;
-  }
+function checkURLAvailability(url, timeout) {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => reject(new Error('Request timed out')), timeout);
+
+    fetch(url, { mode: 'no-cors' })
+      .then(response => {
+        clearTimeout(timer);
+        resolve(response.ok);
+      })
+      .catch(error => {
+        clearTimeout(timer);
+        reject(error);
+      });
+  });
 }
 
 (async function redirectToApp() {
-  const campusAvailable = await checkURLAvailability(campusURL);
-  if (campusAvailable) {
+  try {
+    await checkURLAvailability(campusURL, timeoutDuration);
     window.location.href = campusURL;
-  } else {
+  } catch (error) {
     window.location.href = globalURL;
   }
 })();
